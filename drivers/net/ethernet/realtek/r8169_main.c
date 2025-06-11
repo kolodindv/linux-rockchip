9d2ac3dcc89d2e21a36fb22011320245b8969d7b
kolodindv <kolodindv@rubezh.ru>
2025-06-11 17:18:06 +0400

    add unaligned.h

--- drivers/net/ethernet/realtek/r8169_main.c
+++ drivers/net/ethernet/realtek/r8169_main.c
@@ -28,7 +28,7 @@
 #include <linux/bitfield.h>
 #include <linux/prefetch.h>
 #include <linux/ipv6.h>
-#include <linux/unaligned.h>
+#include <rubezh_kdv/linux/unaligned.h>
 #include <net/ip6_checksum.h>
 #include <net/netdev_queues.h>
 
